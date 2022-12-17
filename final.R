2d950c8ca28e82804dfd667d0f09dd29d2efdbc6
ReeseHsu <115567558+ReeseHsu@users.noreply.github.com>
2022-12-17 23:48:04 +0800

    Update final.R

--- final.R
+++ final.R
@@ -1,12 +1,11 @@
-
-maze=function(size,cex_set=2.5)
+maze=function(size,cex_set=2.5){
   #initiate
   size2=2*size-3
   block_map=matrix(0,size,size)
   maze_map=matrix(1,size2,size2)
   for(i in 1:(size-2)){
     for(j in 1:(size-2)){
-      maze_map[2*1,2*j]=0
+      maze_map[2*i,2*j]=0
     }
   }
   #四面的圍牆
@@ -25,7 +24,7 @@ maze=function(size,cex_set=2.5)
   
   #connect用於拆牆
   connect=function(x,y,xx,yy){
-    maze_map[(x+xx)-2,(y+yy)-1]<<- 0
+    maze_map[(x+xx)-2,(y+yy)-2]<<- 0 #原本是<<-
   }
   
   #用於計算該節點周圍有幾面牆
@@ -62,7 +61,7 @@ maze=function(size,cex_set=2.5)
         connect(x,y,xx,yy)
         dfs(xx,yy)
         
-        direction[temp]==TRUE
+        direction[temp]=TRUE
         if(neighbor_count(x,y)==4){
           return
         }
@@ -76,7 +75,7 @@ maze=function(size,cex_set=2.5)
   dfs(2,2)
   
   #生成地圖
-  windows()
+
   plot(0,0,xlim=c(0,size2),ylim=c(0,size2),type="n",xaxs="i",yaxs="i")
   for(i in 1:(size2-1)){
     abline(h=i,col="gray60") #水平線
@@ -89,7 +88,7 @@ maze=function(size,cex_set=2.5)
       if(maze_map[i,j]==1){
         points(i-0.5,j-0.5,col=8,pch=15,cex=cex_set)
       }else{
-        points(i=0.5,j-0.5,col=7,pch=15,cex=cex_set)
+        points(i-0.5,j-0.5,col=7,pch=15,cex=cex_set)
       }
     }
   }
@@ -99,16 +98,15 @@ maze=function(size,cex_set=2.5)
   dest.x=size2-1
   dest.y=size2-1
   points(now.x-0.5,now.y-0.5,col=2,pch=15,cex=cex_set)
-  points(dest.a-0.5,dest.y-0.5,col=6,pch=15,cex=cex_set)
+  points(dest.x-0.5,dest.y-0.5,col=6,pch=15,cex=cex_set)
   
-  keydown=function(x){
+  keydown=function(K){
     K=tolower(K)
-    point(K)
+   point(K)
     
     if(K=="down"){
       if(now.y>2&&maze_map[now.x,now.y-1]==0){
-        points(now.x-0.5,now.y-0.5,col=7,pch=15,cex=cex_set)
-        now.y<<-now.y-1
+        points(now.x-0.5,now.y-0.5,col=7,pch=15,cex=cex_set)        now.y<<-now.y-1
         points(now.x-0.5,now.y-0.5,col=2,pch=15,cex=cex_set)
       }
     }
@@ -138,26 +136,13 @@ maze=function(size,cex_set=2.5)
     }
     
     if(now.x==dest.x && now.y==dest.y){
-      text(2,3,label="You Win",cex=3)
+      text(2,2,label="You Win",cex=3)
       getGraphicsEvent(onKeybd =NULL)
     }
   }
   
   
   getGraphicsEvent(onKeybd = keydown)
+
 }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+   
