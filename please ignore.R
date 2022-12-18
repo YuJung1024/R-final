261f4980b9c4d8f5c7da948e55da1b650c52667a
YuJung1024 <110306091@g.nccu.edu.tw>
2022-12-18 14:40:54 +0800

    change and add

--- please ignore.R
+++ please ignore.R
@@ -1,3 +1,23 @@
+#install.packages("installr")
+#library(installr)
+#create_windows_desktop_shortcut(run_as_admin=TRUE)
+
+#tcltk
+#gWidgets
+
+# 引入Shiny包
+#install.packages("shiny")
+#library(shiny)
+
+# 定義UI函數
+#ui <- fluidPage(
+# 定義輸入和輸出元素
+# actionButton("move_up", "向上移動"),
+#actionButton("move_left", "向左移動"),
+#actionButton("move_down", "向下移動"),
+#actionButton("move_right", "向右"),
+#)
+
 maze=function(size,cex_set=2.5){
   #initiate
   size2=2*size-3
@@ -5,7 +25,7 @@ maze=function(size,cex_set=2.5){
   maze_map=matrix(1,size2,size2)
   for(i in 1:(size-2)){
     for(j in 1:(size-2)){
-      maze_map[2*i,2*j]=0
+      maze_map[2*1,2*j]=0
     }
   }
   #四面的圍牆
@@ -24,7 +44,7 @@ maze=function(size,cex_set=2.5){
   
   #connect用於拆牆
   connect=function(x,y,xx,yy){
-    maze_map[(x+xx)-2,(y+yy)-2]<<- 0 #原本是<<-
+    maze_map[(x+xx)-2,(y+yy)-1]<<- 0
   }
   
   #用於計算該節點周圍有幾面牆
@@ -61,7 +81,7 @@ maze=function(size,cex_set=2.5){
         connect(x,y,xx,yy)
         dfs(xx,yy)
         
-        direction[temp]=TRUE
+        direction[temp]==TRUE
         if(neighbor_count(x,y)==4){
           return
         }
@@ -75,7 +95,7 @@ maze=function(size,cex_set=2.5){
   dfs(2,2)
   
   #生成地圖
-  
+  windows()
   plot(0,0,xlim=c(0,size2),ylim=c(0,size2),type="n",xaxs="i",yaxs="i")
   for(i in 1:(size2-1)){
     abline(h=i,col="gray60") #水平線
@@ -88,13 +108,16 @@ maze=function(size,cex_set=2.5){
       if(maze_map[i,j]==1){
         points(i-0.5,j-0.5,col=8,pch=15,cex=cex_set)
       }else{
-        points(i-0.5,j-0.5,col=7,pch=15,cex=cex_set)
+        points(i=0.5,j-0.5,col=7,pch=15,cex=cex_set)
       }
     }
   }
   
+  #now紀錄當下位置
   now.x=2
   now.y=2
+  
+  #dest 紀錄終點位置
   dest.x=size2-1
   dest.y=size2-1
   points(now.x-0.5,now.y-0.5,col=2,pch=15,cex=cex_set)
@@ -104,9 +127,11 @@ maze=function(size,cex_set=2.5){
     K=tolower(K)
     point(K)
     
+    #鍵盤事件
     if(K=="down"){
       if(now.y>2&&maze_map[now.x,now.y-1]==0){
-        points(now.x-0.5,now.y-0.5,col=7,pch=15,cex=cex_set)        now.y<<-now.y-1
+        points(now.x-0.5,now.y-0.5,col=7,pch=15,cex=cex_set)
+        now.y<<-now.y-1
         points(now.x-0.5,now.y-0.5,col=2,pch=15,cex=cex_set)
       }
     }
@@ -136,13 +161,27 @@ maze=function(size,cex_set=2.5){
     }
     
     if(now.x==dest.x && now.y==dest.y){
-      text(2,2,label="You Win",cex=3)
+      text(2,3,label="You Win",cex=3)
       getGraphicsEvent(onKeybd =NULL)
     }
   }
   
-  
+  #綁定鍵盤事件
   getGraphicsEvent(onKeybd = keydown)
   
 }
+maze(16)
+
+
+#installr::installr()  
+
+
+
+
+
+
+
+
+
+
 
